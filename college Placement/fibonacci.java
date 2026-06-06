420c57ba467542d3c4d1ea90df42b36f9f051dea
PRAVESH TIWARI <pravesht252@gmail.com>
2026-06-06 22:33:27 +0530

    Fibonacci series

---
+++ college Placement/fibonacci.java
@@ -0,0 +1,26 @@
+public class fibonacci {
+
+    
+
+    
+
+    public static void main(String[] args) {
+
+        int n=6;
+
+        int a=0;
+        int b=1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        for(int i=2;i<n;i++){
+            int c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+            // c=a;
+        }
+        
+    }
+}
